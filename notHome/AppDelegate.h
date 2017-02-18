ae4bb77a93b099e21584db51b02cec1e25cbc700
Artem Belkov <bestK1ngArthur@aol.com>
2017-02-18 18:41:17 +0300

    Initial Commit

---
+++ notHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  notHome
+//
+//  Created by Artem Belkov on 18/02/2017.
+//  Copyright © 2017 BMSTU-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ notHome/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  notHome
+//
+//  Created by Artem Belkov on 18/02/2017.
+//  Copyright © 2017 BMSTU-iOS. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
